86598e471b8eee2e8c357fa8b7b5aa56f62b3f4a
jose-1805 <joseluiscapotemosquera@gmail.com>
2023-06-19 10:44:35 -0500

    Autenticación de usuarios desde servicios aplicado en rutas api.php

--- api.php
+++ api.php
@@ -15,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the 'api' middleware group. Make something great!
 |
 */
-
-Route::post('/logout', [AuthenticationController::class, 'logoutToken']);
-Route::post('/token', [AuthenticationController::class, 'token']);
-Route::get('/background-request-result/{id}/{event}', fn (Request $request, $id, $event) => BackgroundRequest::result($id, $event, $request->user()->id))->middleware('auth:sanctum');
-Route::get('/user-data', [AuthenticationController::class, 'authUserData']);
+// Middleware para autenticar usuarios enviados desde otros servicios
+Route::middleware(['auth_service_user'])->group(function () {
+    Route::post('/logout', [AuthenticationController::class, 'logoutToken']);
+    Route::post('/token', [AuthenticationController::class, 'token']);
+    Route::get('/background-request-result/{id}/{event}', fn (Request $request, $id, $event) => BackgroundRequest::result($id, $event, $request->user()->id))->middleware('auth:sanctum');
+    Route::get('/user-data', [AuthenticationController::class, 'authUserData']);
+});
